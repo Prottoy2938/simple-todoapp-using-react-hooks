631337cb55a1b92422756a00f005d1ee188ab61e
Prottay Rudra <prottoyrudra7777@gmail.com>
2020-01-03 17:31:45 +0600

    Updates

---
+++ precache-manifest.0e58e1b887ff1dcb22560e61da3a4c78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e436d3e4c70dd806909ef597b84d8cfe",
+    "url": "/simple-todoapp-using-react-hooks/index.html"
+  },
+  {
+    "revision": "b07bdc438762c26708b7",
+    "url": "/simple-todoapp-using-react-hooks/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7288ab4fac42ef3ebbd0",
+    "url": "/simple-todoapp-using-react-hooks/static/js/2.e8ff6a49.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/simple-todoapp-using-react-hooks/static/js/2.e8ff6a49.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b07bdc438762c26708b7",
+    "url": "/simple-todoapp-using-react-hooks/static/js/main.8827ddcc.chunk.js"
+  },
+  {
+    "revision": "2183ecfc840f02d94824",
+    "url": "/simple-todoapp-using-react-hooks/static/js/runtime-main.8f48c512.js"
+  }
+]);
